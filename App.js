43bad52dc1ca6bcc91d2801a72b7e97515d6c4cb
Dillon Coffman <dillonscoffman@gmail.com>
2019-02-26 18:25:21 -0500

    Touched up syntax with eslint

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { 
+import React, { Component } from 'react';
+import {
   StyleSheet,
   Text,
   Button,
@@ -10,123 +10,114 @@ import {
   Dimensions,
   Easing,
   TouchableOpacity,
-  SafeAreaView // for larger screens like iPhone X
+  SafeAreaView, // for larger screens like iPhone X
 } from 'react-native';
-import { 
+import {
   createAppContainer, // explicit app container required for navigators as of react-navigation v3
   createSwitchNavigator,
   createBottomTabNavigator,
-  createStackNavigator
-} from 'react-navigation'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import FeatherIcons from 'react-native-vector-icons/Feather'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import HomeScreen from './screens/Home'
-import LoginScreen from './screens/Login'
-import ProfileScreen from './screens/Profile'
-import SearchScreen from './screens/Search'
-import StreamScreen from './screens/Stream'
-
+  createStackNavigator,
+} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import HomeScreen from './screens/Home';
+import LoginScreen from './screens/Login';
+import ProfileScreen from './screens/Profile';
+import SearchScreen from './screens/Search';
+import StreamScreen from './screens/Stream';
 
 // Get width and height depending on device
-const SCREEN_WIDTH = Dimensions.get("window").width
-const SCREEN_HEIGHT = Dimensions.get("window").height
-
-
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 // Song screen for immediate playing
 class SongScreen extends Component {
-
   static navigationOptions = {
-    header: null // gets rid of header nav if playing song
-  }
+    header: null, // gets rid of header nav if playing song
+  };
 
-   // PanResponder Animation
+  // PanResponder Animation
 
-   componentWillMount () {
-    this.animation = new Animated.ValueXY({ x: 0, y: 0  })
+  componentWillMount() {
+    this.animation = new Animated.ValueXY({ x: 0, y: 0 });
     this.PanResponder = PanResponder.create({
-
-      onMoveShouldSetPanResponder: (evt, gestureState)=> true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderMove: (evt, gestureState) => {
-        this.animation.setValue({x: 0, y: gestureState.dy})
+        this.animation.setValue({ x: 0, y: gestureState.dy });
       },
       onPanResponderRelease: (evt, gestureState) => {
-        if(gestureState.moveY < SCREEN_HEIGHT && gestureState.dy < 0) {
+        if (gestureState.moveY < SCREEN_HEIGHT && gestureState.dy < 0) {
           Animated.spring(this.animation.y, {
             toValue: 0,
-            tension: 1
-          }).start()
-        }
-        else if(gestureState.dy < 0) {
+            tension: 1,
+          }).start();
+        } else if (gestureState.dy < 0) {
           Animated.spring(this.animation.y, {
             toValue: 0,
-            tension: 1
-          }).start()
-        }
-        else if(gestureState.dy > 0) {
+            tension: 1,
+          }).start();
+        } else if (gestureState.dy > 0) {
           Animated.spring(this.animation.y, {
             toValue: SCREEN_HEIGHT - 60,
-            tension: 1
-          }).start()
+            tension: 1,
+          }).start();
         }
-      } 
-    })
+      },
+    });
   }
 
-
-  constructor () {
-    super()
-    this.animatedValue = new Animated.Value(0)
+  constructor() {
+    super();
+    this.animatedValue = new Animated.Value(0);
   }
 
-  componentDidMount () {
-    this.animate()
+  componentDidMount() {
+    this.animate();
   }
 
   animate() {
-    this.animatedValue.setValue(0)
-    Animated.timing( // see Animated API for more info, this just animates a value over time using an easing function
+    this.animatedValue.setValue(0);
+    Animated.timing(
+      // see Animated API for more info, this just animates a value over time using an easing function
       this.animatedValue,
       {
         toValue: 1,
         duration: 90000,
-        easing: Easing.linear
+        easing: Easing.linear,
       }
-    ).start(() => this.animate()) // loops the animation by repeating above Animated.timing()
+    ).start(() => this.animate()); // loops the animation by repeating above Animated.timing()
   }
 
   render() {
-    marginLeft = this.animatedValue.interpolate({
+    const marginLeft = this.animatedValue.interpolate({
       inputRange: [0, 1], // think of as 0% to 100%, we scroll starting at left margin til SCREEN_WIDTH reached at 100%
-      outputRange: [0, -SCREEN_WIDTH * 2]
-    })
+      outputRange: [0, -SCREEN_WIDTH * 2],
+    });
 
     return (
       <SafeAreaView>
         <View style={styles.container}>
-          <Button 
-            title='Im the SongScreen!'
+          <Button
+            title="Im the SongScreen!"
             onPress={() => this.props.navigation.navigate('Home')}
           />
         </View>
 
         <Animated.View style={styles.imageContainer}>
-          <Animated.Image 
+          <Animated.Image
             source={require('./images/cloud.jpg')}
             style={{
               marginLeft,
               height: SCREEN_HEIGHT,
               width: SCREEN_WIDTH * 3,
-              position: 'absolute'
+              position: 'absolute',
             }}
           />
 
           <Animated.View style={styles.upperMostIcons}>
-            <TouchableOpacity>
-            
-            </TouchableOpacity>
+            <TouchableOpacity />
 
             <View>
               <TouchableOpacity>
@@ -138,14 +129,10 @@ class SongScreen extends Component {
               </TouchableOpacity>
             </View>
 
-            <TouchableOpacity>
-              
-            </TouchableOpacity>
-
+            <TouchableOpacity />
           </Animated.View>
-          
-          <Animated.View style={styles.upperIcons}>
 
+          <Animated.View style={styles.upperIcons}>
             <Animated.View style={styles.upperLeftIcons}>
               <TouchableOpacity>
                 <Text style={styles.artistName}>Morgan Powers</Text>
@@ -158,39 +145,54 @@ class SongScreen extends Component {
 
             <Animated.View>
               <TouchableOpacity>
-                <MaterialCommunityIcons name='chevron-down' size={40} style={styles.upperRightIcon} />
+                <MaterialCommunityIcons
+                  name="chevron-down"
+                  size={40}
+                  style={styles.upperRightIcon}
+                />
               </TouchableOpacity>
             </Animated.View>
-
           </Animated.View>
 
           <Animated.View style={styles.lowerIcons}>
             <Animated.View style={styles.lowerIconsWrapper}>
-
               <TouchableOpacity style={styles.likes}>
-                <MaterialCommunityIcons name={'heart'} size={25} style={{color: 'white'}} />
-                <Text style={{color: 'white'}}>1,414</Text>
+                <MaterialCommunityIcons
+                  name="heart"
+                  size={25}
+                  style={{ color: 'white' }}
+                />
+                <Text style={{ color: 'white' }}>1,414</Text>
               </TouchableOpacity>
 
               <TouchableOpacity>
-                <FeatherIcons name={'share'} size={25} style={{color: 'white'}} />
+                <FeatherIcons
+                  name="share"
+                  size={25}
+                  style={{ color: 'white' }}
+                />
               </TouchableOpacity>
 
               <TouchableOpacity>
-                <MaterialCommunityIcons name={'repeat-once'} size={25} style={{color: 'white'}} />
+                <MaterialCommunityIcons
+                  name="repeat-once"
+                  size={25}
+                  style={{ color: 'white' }}
+                />
               </TouchableOpacity>
 
               <TouchableOpacity>
-                <MaterialIcons name={'more-horiz'} size={35} style={{color: 'white'}} />
+                <MaterialIcons
+                  name="more-horiz"
+                  size={35}
+                  style={{ color: 'white' }}
+                />
               </TouchableOpacity>
-
             </Animated.View>
           </Animated.View>
-
-
         </Animated.View>
       </SafeAreaView>
-    )
+    );
   }
 }
 
@@ -199,81 +201,81 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   headerIcon: {
-    marginLeft: 10, 
-    marginRight: 135
+    marginLeft: 10,
+    marginRight: 135,
   },
   headerText: {
-    fontSize: 18, 
-    color: '#444', 
+    fontSize: 18,
+    color: '#444',
   },
   imageContainer: {
     position: 'absolute',
-    left: 0, 
-    right: 0, 
-    height: SCREEN_HEIGHT
+    left: 0,
+    right: 0,
+    height: SCREEN_HEIGHT,
   },
   upperMostIcons: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    justifyContent: 'space-between', 
-    margin: 12
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 12,
   },
   songNameAndArtistName: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   UpperSongTitle: {
-    color: 'white'
+    color: 'white',
   },
   upperArtistName: {
-    color: 'white', 
-    textDecorationLine: 'underline', 
-    fontWeight: 'bold'
+    color: 'white',
+    textDecorationLine: 'underline',
+    fontWeight: 'bold',
   },
   upperIcons: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   upperLeftIcons: {
-    flexDirection: 'column' 
+    flexDirection: 'column',
   },
   artistName: {
-    backgroundColor: '#000', 
-    color: '#fff', 
-    marginLeft: 15, 
-    fontSize: 20, 
-    fontWeight: 'bold', 
-    textDecorationLine: 'underline'
+    backgroundColor: '#000',
+    color: '#fff',
+    marginLeft: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
   },
   songTitle: {
-    backgroundColor: '#000', 
-    color: '#fff', 
-    marginLeft: 15, 
-    marginTop: 10, 
-    fontSize: 25, 
-    fontWeight: 'bold'
+    backgroundColor: '#000',
+    color: '#fff',
+    marginLeft: 15,
+    marginTop: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
   },
   upperRightIcon: {
-    color: '#fff', 
-    marginRight: 10
+    color: '#fff',
+    marginRight: 10,
   },
   lowerIcons: {
-    flex: 1, 
-    justifyContent: 'flex-end', 
-    marginBottom: 40 
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 40,
   },
   lowerIconsWrapper: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-around',
   },
   likes: {
-    flexDirection: 'row', 
-    alignItems: 'center'
-  }
-})
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 /* 
 Tab navigator navigationOptions can only be changed by the stack navigators 
@@ -282,104 +284,131 @@ as of react-navigation v3, hence the need for a bunch of Stack Navigators
 
 const HomeStackNav = createStackNavigator({
   Home: HomeScreen,
-  Song: SongScreen
-})
+  Song: SongScreen,
+});
 const StreamStackNav = createStackNavigator({
   Stream: StreamScreen,
-  Song: SongScreen
-})
+  Song: SongScreen,
+});
 const SearchStackNav = createStackNavigator({
   Search: SearchScreen,
-  Song: SongScreen
-})
+  Song: SongScreen,
+});
 const ProfileStackNav = createStackNavigator({
   Profile: ProfileScreen,
-  Song: SongScreen
-})
+  Song: SongScreen,
+});
 
 HomeStackNav.navigationOptions = ({ navigation }) => {
-  let { routeName } = navigation.state.routes[navigation.state.index];
-  let navigationOptions = {};
+  const { routeName } = navigation.state.routes[navigation.state.index];
+  const navigationOptions = {};
 
-  if(routeName === 'Song') { // gets rid of tab nav if playing song
-    navigationOptions.tabBarVisible = false
-  } 
+  if (routeName === 'Song') {
+    // gets rid of tab nav if playing song
+    navigationOptions.tabBarVisible = false;
+  }
 
   if (routeName === 'Home') {
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='home' size={25} color='black' />;
-  } else { // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='home' size={25} color='black' />;
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="home" size={25} color="black" />
+    );
+  } else {
+    // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="home" size={25} color="black" />
+    );
   }
 
   return navigationOptions;
-}
+};
 
 StreamStackNav.navigationOptions = ({ navigation }) => {
-  let { routeName } = navigation.state.routes[navigation.state.index];
-  let navigationOptions = {};
+  const { routeName } = navigation.state.routes[navigation.state.index];
+  const navigationOptions = {};
 
-  if(routeName === 'Song') { // gets rid of tab nav if playing song
-    navigationOptions.tabBarVisible = false
-  } 
+  if (routeName === 'Song') {
+    // gets rid of tab nav if playing song
+    navigationOptions.tabBarVisible = false;
+  }
 
   if (routeName === 'Stream') {
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='flash' size={25} color='black' />;
-  } else { // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='flash' size={25} color='black' />;
-  } 
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="flash" size={25} color="black" />
+    );
+  } else {
+    // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="flash" size={25} color="black" />
+    );
+  }
 
   return navigationOptions;
-}
+};
 
 SearchStackNav.navigationOptions = ({ navigation }) => {
-  let { routeName } = navigation.state.routes[navigation.state.index];
-  let navigationOptions = {};
+  const { routeName } = navigation.state.routes[navigation.state.index];
+  const navigationOptions = {};
 
-  if(routeName === 'Song') { // gets rid of tab nav if playing song
-    navigationOptions.tabBarVisible = false
-  } 
+  if (routeName === 'Song') {
+    // gets rid of tab nav if playing song
+    navigationOptions.tabBarVisible = false;
+  }
 
   if (routeName === 'Search') {
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialIcons name='search' size={25} color='black' />;
-  } else { // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialIcons name='search' size={25} color='black' />;
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialIcons name="search" size={25} color="black" />
+    );
+  } else {
+    // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialIcons name="search" size={25} color="black" />
+    );
   }
 
   return navigationOptions;
-}
+};
 
 ProfileStackNav.navigationOptions = ({ navigation }) => {
-  let { routeName } = navigation.state.routes[navigation.state.index];
-  let navigationOptions = {};
+  const { routeName } = navigation.state.routes[navigation.state.index];
+  const navigationOptions = {};
 
-  if(routeName === 'Song') { // gets rid of tab nav if playing song
-    navigationOptions.tabBarVisible = false
-  } 
+  if (routeName === 'Song') {
+    // gets rid of tab nav if playing song
+    navigationOptions.tabBarVisible = false;
+  }
 
   if (routeName === 'Profile') {
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='account' size={25} color='black' />;
-  } else { // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
-    navigationOptions.tabBarIcon = ({ focused }) => <MaterialCommunityIcons name='account' size={25} color='black' />;
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="account" size={25} color="black" />
+    );
+  } else {
+    // placeholder for now to ensure when going to SongScreen that Home icon doesn't disappear
+    navigationOptions.tabBarIcon = ({ focused }) => (
+      <MaterialCommunityIcons name="account" size={25} color="black" />
+    );
   }
 
   return navigationOptions;
-}
+};
 
 // Create Tab Navigator using Stack Navigators
 const Tabs = createBottomTabNavigator({
   Home: HomeStackNav,
   Stream: StreamStackNav,
   Search: SearchStackNav,
-  Profile: ProfileStackNav
-})
+  Profile: ProfileStackNav,
+});
 
 // Switch Navigator to show only one screen at a time
-const Switch = createSwitchNavigator({
-  Login: LoginScreen, // route config
-  Home: Tabs // allow Home to go to any one of the Tabs configured above
-}, {
-  initialRouteName: 'Login' // starting screen
-});
+const Switch = createSwitchNavigator(
+  {
+    Login: LoginScreen, // route config
+    Home: Tabs, // allow Home to go to any one of the Tabs configured above
+  },
+  {
+    initialRouteName: 'Login', // starting screen
+  }
+);
 
 const App = createAppContainer(Switch); // wrap Switch in app container for react-navigation v3
 
